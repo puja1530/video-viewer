b0d343435096a9614db29ab2018c2328ff6c251a
puja1530 <68124729+puja1530@users.noreply.github.com>
2021-03-11 00:48:50 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
-$connect=mysqli_connect("localhost","root","","videoviewer"); //Connecting to database server
+$connect=mysqli_connect('sql210.epizy.com','epiz_28124114','ivUwfWEJ25js7Y','epiz_28124114_videoviewer'); //Connecting to database server
 if($connect->connect_error)
 {
 	die("Connection failed :".$connect->connect_error);
